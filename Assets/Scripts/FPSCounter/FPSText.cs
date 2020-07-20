84ec78ded3b2129624d703fde0b42c8787feb390
Maciej Mucha <mmucha.00@gmail.com>
2020-07-20 11:08:58 +0200

    fps counter

---
+++ Assets/Scripts/FPSCounter/FPSText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSText : MonoBehaviour
+{
+    private Text text;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        text.text = $"{Math.Round(Time.smoothDeltaTime * 1000f, 1)}ms";
+    }
+}
